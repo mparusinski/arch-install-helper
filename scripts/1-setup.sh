d07a4350324970874308c976fe22eaf28edd4b85
Michal Parusinski <michal@parusinski.me>
2024-01-03 10:57:53 +0000

    Setup script

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#github-action genshdoc
+# vim: noai:ts=4:sw=4:expandtab
 #
 # @file Setup
 # @brief Configures installed system
@@ -11,9 +11,58 @@ CONFIGURING ARCHLINUX SYSTEM
 "
 source $HOME/infrastructure/configs/setup.conf
 
+echo -ne "
+-------------------------------------------------------------------------------
+1. Updating base system ... 
+-------------------------------------------------------------------------------
+"
+# Keyring must be updated before performing upgrade otherwise we get complains
+# about corrupted packages
+pacman -Sy --noconfirm archlinux-keyring
+pacman -Su
 
 echo -ne "
-1. Updating base system ... 
+-------------------------------------------------------------------------------
+2. Setting up mirrors for optimal download
+-------------------------------------------------------------------------------
 "
-pacman -S --noconfirm archlinux-keyring
+REFLECTOR_CONF=/etc/xdg/reflector/reflector.conf
+pacman -S --noconfirm --needed pacman-contrib curl
+pacman -S --noconfirm --needed reflector
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.back
+sed -i "$ a # Specify desired countries" $REFLECTOR_CONF
+sed -i "$ a --country France,Germany,Switzerland" $REFLECTOR_CONF
+systemctl enable --now reflector
+
+echo -ne "
+-------------------------------------------------------------------------------
+3. Configuring SSH server (if needed)
+-------------------------------------------------------------------------------
+"
+if [[ ! -z "$SERVER" ]]; then
+    SSHD_CONFIG=/etc/ssh/sshd_config
+    sed -iE "s/#?Port 22/Port 2222/gm" $SSHD_CONFIG
+    sed -iE "s/#?PermitRootLogin (no|yes)/PermitRootLogin no/gm" $SSHD_CONFIG
+    sed -iE "s/#?PasswordAuthentication (no|yes)/PasswordAuthentication no/gm" $SSHD_CONFIG
+fi
+
+echo -ne "
+-------------------------------------------------------------------------------
+3. Setting up users
+-------------------------------------------------------------------------------
+"
+USERNAME=michalparusinski
+pacman -S --noconfirm zsh vim
+echo "Please check wheel group can run sudo commands"
+read -n 1 -s
+EDITOR=vim visudo
+useradd -m -G wheel -s /bin/zsh $USERNAME
+echo "Please enter password"
+read -n 1 -s
+passwd $USERNAME
+if [[ ! -z "$SERVER" ]]; then
+    mkdir -p /home/$USERNAME/.ssh/
+    cp configs/authorized_keys /home/$USERNAME/.ssh/authorized_keys
+    chown $USERNAME /home/$USERNAME/.ssh/authorized_keys
+fi
 
